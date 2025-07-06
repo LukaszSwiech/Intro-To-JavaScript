7e7a4f1fce1f0b4626879d202aef42c511dd47d2
hyper <hyperskill@hyperskill.org>
2025-07-06 16:52:35 +0200

    Stage2: count and print number of needed ingredients

--- Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
+++ Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
@@ -2,12 +2,13 @@
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
 const input = require('sync-input')
+let amountOfWater = 200;
+let amountOfMilk = 50;
+let amountOfCoffeeBeans = 15;
 
-let coffeeMachineText = `Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`
-console.log(coffeeMachineText)
+let numberOfCoffeeDrinks = Number(input("Write how many cups of coffee you will need:" ))
+
+console.log(`For ${numberOfCoffeeDrinks} cups of coffee you will need:
+${amountOfWater * numberOfCoffeeDrinks} ml of water
+${amountOfMilk * numberOfCoffeeDrinks} ml of milk
+${amountOfCoffeeBeans * numberOfCoffeeDrinks} g of coffee beans`);
