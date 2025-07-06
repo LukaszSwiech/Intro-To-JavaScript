0fe2d57c6bacc194ef49af01952e405a1f93d924
hyper <hyperskill@hyperskill.org>
2025-07-06 16:45:18 +0200

    Stage1: Print coffee machine text

--- Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
+++ Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
@@ -3,4 +3,11 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-console.log("Hello, World!")
+let coffeeMachineText = `Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!`
+console.log(coffeeMachineText)
