f781ca89be2909d77771c00dfbb8f07e31612cd6
hyper <hyperskill@hyperskill.org>
2025-07-06 17:19:54 +0200

    Stage3: measure max coffees to prepare

--- Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
+++ Coffee Machine (JavaScript)/Coffee Machine (JavaScript)/task/main.js
@@ -2,13 +2,38 @@
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
 const input = require('sync-input')
-let amountOfWater = 200;
-let amountOfMilk = 50;
-let amountOfCoffeeBeans = 15;
+
+let maxCoffees = Infinity;
+let ingredients = {
+    water: 200,
+    milk: 50,
+    coffeeBeans: 15
+}
+let currentSupply = {
+    water: Number(input("Write how many ml of water the coffee machine has:")),
+    milk: Number(input("Write how many ml of milk the coffee machine has:")),
+    coffeeBeans:Number(input("Write how many grams of coffee beans the coffee machine has:"))
+}
+
+for (let ingredient in ingredients) {
+    if (Math.floor(currentSupply[ingredient] / ingredients[ingredient]) < maxCoffees)
+    {
+        maxCoffees = Math.floor(currentSupply[ingredient] / ingredients[ingredient]);
+        console.log(maxCoffees);
+    }
+}
 
 let numberOfCoffeeDrinks = Number(input("Write how many cups of coffee you will need:" ))
 
-console.log(`For ${numberOfCoffeeDrinks} cups of coffee you will need:
-${amountOfWater * numberOfCoffeeDrinks} ml of water
-${amountOfMilk * numberOfCoffeeDrinks} ml of milk
-${amountOfCoffeeBeans * numberOfCoffeeDrinks} g of coffee beans`);
+if (numberOfCoffeeDrinks === maxCoffees)
+{
+  console.log(`Yes, I can make that amount of coffee`);
+}
+else if (numberOfCoffeeDrinks < maxCoffees)
+{
+    console.log(`Yes, I can make that amount of coffee (and even ${maxCoffees - numberOfCoffeeDrinks} more than that)`);
+}
+else
+{
+    console.log(`No, I can make only ${maxCoffees} cups of coffee`);
+}
