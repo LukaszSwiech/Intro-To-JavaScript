fa0186dce81096db79d239c3768264819b3219c8
hyper <hyperskill@hyperskill.org>
2025-07-06 16:31:24 +0200

    Stage2: take input from user

--- Chalkboard Printer/Chalkboard Printer/task/task.js
+++ Chalkboard Printer/Chalkboard Printer/task/task.js
@@ -1,17 +1,11 @@
 const input = require('sync-input');
 
-let name = "Bart";
-let surname = "Simpson";
-let message = "I will not skateboard in the halls.";
+let name = input("Enter name: ");
+let surname = input("Enter surname: ");
+let message = input("Enter message:");
 
 for (let i = 0; i < 5; i++) {
     console.log(`This is ${name} ${surname} and ${message}`);
 }
 
 
-// We have also imported the 'sync-input' package for you.
-// You will use this in later stages.
-// This package allows you to get user input.
-// Like so:
-// let name = input("Type your name: ");
-// let age = Number(input("Type your age: "));
