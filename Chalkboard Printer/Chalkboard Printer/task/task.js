66ff63cbc8a2cf441d4aaa8ee213729213aabcf3
hyper <hyperskill@hyperskill.org>
2025-07-06 16:33:00 +0200

    Stage3: take input from user and repeat x times

--- Chalkboard Printer/Chalkboard Printer/task/task.js
+++ Chalkboard Printer/Chalkboard Printer/task/task.js
@@ -3,9 +3,8 @@ const input = require('sync-input');
 let name = input("Enter name: ");
 let surname = input("Enter surname: ");
 let message = input("Enter message:");
+let repeats = Number(input("Enter number of repeats:"));
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < repeats; i++) {
     console.log(`This is ${name} ${surname} and ${message}`);
 }
-
-
