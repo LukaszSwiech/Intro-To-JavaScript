826ca9d0984e59158a231364360ffc625781d264
hyper <hyperskill@hyperskill.org>
2025-07-07 16:57:22 +0200

    Stage2: Select currency and convert

--- Simple Currency Converter/Simple Currency Converter/task/main.js
+++ Simple Currency Converter/Simple Currency Converter/task/main.js
@@ -17,9 +17,42 @@ let exchangeRates = {
 
 console.log("Welcome to Currency Converter!")
 printExchangeRate(exchangeRates);
+console.log(`I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP`);
+console.log("Type the currency you wish to convert: USD")
+let selectedCurrency = input("To:").toUpperCase()
+if (!Object.keys(exchangeRates).includes(selectedCurrency) ) {
+    console.log("Unknown currency")
+    return
+}
+let amountToExchange = Number(input("Amount:"))
+
+if (isNaN(amountToExchange)) {
+    console.log('The amount has to be a number')
+}
+else if (amountToExchange < 1) {
+    console.log('The amount cannot be less than 1')
+}
+else {
+    console.log(`Result: ${amountToExchange} USD equals ${(amountToExchange * exchangeRates[selectedCurrency]).toFixed(4)} ${selectedCurrency}`);
+}
+
+
+function printAvailCurrencies(exchangeRate) {
+    process.stdout.write("I can convert USD to these currencies: ")
+   Object.entries(exchangeRate).forEach(([key, value], index) => {
+/*       if (key === "USD") {
+           return
+       }*/
+       if (index === Object.entries(exchangeRate).length - 1) {
+           process.stdout.write(`${key}`);
+       } else {
+           process.stdout.write(`${key}, `);
+       }
+})
+}
 
 function printExchangeRate(exchangeRate) {
-   Object.entries(exchangeRate).forEach(([key, value]) => {
-       console.log(`1 USD equals ${value} ${key}`);
-   })
+    Object.entries(exchangeRate).forEach(([key, value]) => {
+        console.log(`1 USD equals ${value} ${key}`);
+    })
 }
