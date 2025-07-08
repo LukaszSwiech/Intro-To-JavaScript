a84ae6e0767b37146887a7db4957615ca78e8278
hyper <hyperskill@hyperskill.org>
2025-07-08 21:56:05 +0200

    Stage5: Hangman draft version

--- Hangman (JavaScript)/Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/Hangman (JavaScript)/task/main.js
@@ -1,6 +1,61 @@
-// Use "input()" to input a line from the user
-// Use "input(str)" to print some text before requesting input
-// You will need this in the following stages
 const input = require('sync-input')
+let userInput
+let guess
+let guessCheck
+let index = 0
+let game = {
+    name: "H A N G M A N",
+    lives:  8,
+    words: ["python", "java", "swift", "javascript"],
+    finalMessage: "Thanks for playing!"
+}
 
-console.log("Hello, World!")
+gamePrepare()
+gameIntro()
+
+do {
+
+    guess = gameIteration();
+    guessCheck = checkAnswer(guess);
+    guessCheck ? correctAnswer(guess, game.correctAnswer) : wrongAnswer()
+
+    console.log(`//  ${--game.lives} attempts`)
+
+} while (game.lives > 0)
+
+console.log(game.finalMessage)
+
+
+function gamePrepare() {
+    const getRandomAnswer = () => game.words[Math.floor(Math.random() * game.words.length)];
+    game.correctAnswer = getRandomAnswer();
+    game.currentState = "-".repeat(game.correctAnswer.length);
+}
+
+function gameIntro() {
+    console.log(game.name)
+    console.log(`//  ${game.lives} attempts`)
+}
+
+function gameIteration () {
+    console.log(game.currentState)
+    return userInput = input("Input a letter:");
+}
+
+function checkAnswer(answer) {
+    return game.correctAnswer.includes(answer);
+}
+
+function correctAnswer(guess, correctAnswer) {
+    index = game.correctAnswer.search(guess);
+     do {
+        game.currentState = game.currentState.substring(0, index) + guess + game.currentState.substring(index + 1)
+        correctAnswer = correctAnswer.substring(0, index) + "-" + correctAnswer.substring(index + 1)
+        index = correctAnswer.search(guess);
+    } while (index >= 0)
+
+}
+
+function wrongAnswer() {
+    console.log(`That letter doesn't appear in the word.`)
+}
