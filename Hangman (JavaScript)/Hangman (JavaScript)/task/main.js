10d9be369e039b76d47f7ae0f96a5157dc91d98b
hyper <hyperskill@hyperskill.org>
2025-07-13 21:08:49 +0200

    Refactor. Split main game into functions. Use the second object to store game stats.

--- Hangman (JavaScript)/Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/Hangman (JavaScript)/task/main.js
@@ -1,6 +1,5 @@
 const input = require('sync-input');
-let guess;
-let choice;
+
 const HANGMAN_GAME = {
     name: "H A N G M A N",
     initialLives: 8,
@@ -10,6 +9,7 @@ const HANGMAN_GAME = {
         lost: 0
     }
 }
+
 let gameState = {
     lives: HANGMAN_GAME.initialLives,
     currentWord: "",
@@ -22,110 +22,137 @@ const displayGameIntro = () => {
     console.log();
 }
 
-const initializeGame = () => {
-    const getRandomAnswer = () => HANGMAN_GAME.words[Math.floor(Math.random() * HANGMAN_GAME.words.length)];
-    HANGMAN_GAME.correctAnswer = getRandomAnswer();
-    HANGMAN_GAME.currentState = new Array(HANGMAN_GAME.correctAnswer.length).fill('-');
+const getRandomWord = () => {
+    return HANGMAN_GAME.words[Math.floor(Math.random() * HANGMAN_GAME.words.length)];
 }
 
-const getUserInput = () => {
-    console.log(HANGMAN_GAME.currentState.join(''));
+const initializeGame = () => {
+    gameState.currentWord = getRandomWord();
+    gameState.guessedLetters = [];
+    gameState.currentProgress = new Array(gameState.currentWord.length).fill('-');
+    gameState.lives = HANGMAN_GAME.initialLives;
+}
+
+const getUserGuess = () => {
+    console.log(gameState.currentProgress.join(''));
     return input("Input a letter: ");
 }
 
-const checkAnswer = (answer) => {
-    return HANGMAN_GAME.correctAnswer.includes(answer);
+const isValidGuess = (guess) => {
+    if (guess.length !== 1) {
+        console.log("Please, input a single letter.")
+        return false;
+    }
+    if (!/^[a-z]$/.test(guess)) {
+        console.log("Please, enter a lowercase letter from the English alphabet.")
+        return false;
+    }
+    if (gameState.guessedLetters.includes(guess)) {
+        console.log(`You've already guessed this letter.`)
+        return false;
+    }
+    return true;
 }
 
 const processCorrectGuess = (guess) => {
-    // Find all positions of the guessed letter
-    for (let i = 0; i < HANGMAN_GAME.correctAnswer.length; i++) {
-        if (HANGMAN_GAME.correctAnswer[i] === guess) {
-            HANGMAN_GAME.currentState[i] = guess;
+    for (let i = 0; i < gameState.currentWord.length; i++) {
+        if (gameState.currentWord[i] === guess) {
+            gameState.currentProgress[i] = guess;
         }
     }
 }
 
 const processIncorrectGuess = () => {
     console.log(`That letter doesn't appear in the word.`);
-    HANGMAN_GAME.initialLives--;
+    gameState.lives--;
 }
 
-const checkUserInputLength = (userInput) => {
-    return (userInput.length > 1)
+const isWordComplete = () => {
+    return gameState.currentProgress.join('') === gameState.currentWord;
 }
 
-const checkLowerCase = (userInput) => {
-    return !/^[a-z]$/.test(userInput)
+const displayGameStatus = () => {
+    const attemptText = gameState.lives === 1 ? "attempt" : "attempts";
+    console.log(`// ${gameState.lives} ${attemptText}`)
+    console.log();
+}
+
+const handleLoss = () => {
+    console.log('You lost!');
+    HANGMAN_GAME.statistics.lost++;
+}
+
+const handleWin = () => {
+    console.log(`You guessed the word ${gameState.currentWord}!`)
+    console.log('You survived!')
+    HANGMAN_GAME.statistics.won++;
 }
 
 const displayResults = () => {
-    console.log(`you won: ${HANGMAN_GAME.statistics.won} times.`);
-    console.log(`you lost: ${HANGMAN_GAME.statistics.lost} times.`);
+    console.log(`you won: ${HANGMAN_GAME.statistics.won} times.`)
+    console.log(`you lost: ${HANGMAN_GAME.statistics.lost} times.`)
+}
+
+const playGame = () => {
+    initializeGame();
+
+    while (gameState.lives > 0) {
+        const guess = getUserGuess();
+
+        if (!isValidGuess(guess)) {
+            continue
+        }
+
+        gameState.guessedLetters.push(guess);
+
+        if (gameState.currentWord.includes(guess)) {
+            processCorrectGuess(guess);
+
+            if (isWordComplete()) {
+                handleWin();
+                return;
+            }
+
+        } else {
+            processIncorrectGuess();
+        }
+
+        displayGameStatus();
+    }
+    handleLoss();
 }
 
 const getMenuChoice = () => {
     return input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:');
 }
-const handleLoss= () => {
-    console.log('You lost!')
-    HANGMAN_GAME.lost++;
-}
 
-displayGameIntro();
 
-let isRunning = true;
-while (isRunning) {
+const runGame = () => {
 
-    choice = getMenuChoice()
+    displayGameIntro();
 
-    switch (choice) {
-        case "results":
-            displayResults()
-            break;
-        case "exit":
-            isRunning = false;
-            break;
-        case "play":
-            initializeGame()
-            do {
-                guess = getUserInput();
+    let isRunning = true;
 
-                if (checkUserInputLength(guess)) {
-                    console.log("Please, input a single letter.")
-                    continue
-                }
+    while (isRunning) {
+        const choice = getMenuChoice();
 
-                if (checkLowerCase(guess)) {
-                    console.log("Please, enter a lowercase letter from the English alphabet.")
-                    continue
-                }
+        switch (choice) {
+            case "results":
+                displayResults()
+                break;
 
-                if (HANGMAN_GAME.currentState.includes(guess)) {
-                    console.log(`You've already guessed this letter.`)
-                    continue;
-                }
-                (checkAnswer(guess)) ? processCorrectGuess(guess) : processIncorrectGuess();
+            case "exit":
+                isRunning = false;
+                break;
 
-                if (HANGMAN_GAME.currentState.join('') === HANGMAN_GAME.correctAnswer) {
-                    console.log(`You guessed the word ${HANGMAN_GAME.correctAnswer}!`)
-                    console.log('You survived!')
-                    HANGMAN_GAME.won++;
-                    break;
-                }
+            case "play":
+                playGame();
+                break;
 
-                const attemptText = HANGMAN_GAME.initialLives === 1 ? "attempt" : "attempts";
-                console.log(`//  ${HANGMAN_GAME.initialLives} ${attemptText}`);
-                console.log();
-
-            } while (HANGMAN_GAME.initialLives > 0);
-
-            if (HANGMAN_GAME.initialLives === 0) {
-                handleLoss()
-            }
-            break;
-        default:
-            break;
-
+            default:
+                break;
+        }
     }
 }
+
+runGame();
